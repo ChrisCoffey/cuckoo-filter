a78e052029d80247ac4571c8e390a5c81a6a3d87
Chris Coffey <cmcof273@gmail.com>
2018-10-21 21:31:14 -0400

    Cut over to a more efficient bucket strategy

--- src/Data/CuckooFilter.hs
+++ src/Data/CuckooFilter.hs
@@ -17,7 +17,7 @@ module Data.CuckooFilter
     ) where
 
 import Data.Aeson (ToJSON, FromJSON)
-import Data.Bits (xor)
+import Data.Bits (xor, (.&.), (.|.), shiftR, shiftL)
 import Data.Hashable (Hashable, hash)
 import qualified Data.IntMap.Strict as IM
 import Data.Serialize (Serialize)
@@ -59,16 +59,38 @@ newtype FingerPrint = FP Word8
     deriving (Show, Eq, Ord, Generic)
     deriving newtype (ToJSON, FromJSON, Hashable)
     deriving anyclass Serialize
+emptyFP :: FingerPrint
+emptyFP = FP 0
 -- | A Bucket is a statically sized list of four FingerPrints.
 --
--- TODO use a Word32 instead of a 4-tuple
-newtype Bucket = B (FingerPrint, FingerPrint, FingerPrint, FingerPrint)
+newtype Bucket = B Word32
     deriving (Show, Eq, Ord)
     deriving stock Generic
     deriving newtype (ToJSON, FromJSON)
     deriving anyclass Serialize
 emptyBucket :: Bucket
-emptyBucket = B (FP 0, FP 0, FP 0, FP 0)
+emptyBucket = B 0
+
+getCell ::
+    Bucket
+    -> Natural -- Really just 0-3. Is it worth creating a custom datatype for this?
+    -> FingerPrint
+getCell (B bucket) cellNumber =
+    FP . fromIntegral $ (bucket .&. mask) `shiftR` offset
+    where
+        offset = (fromIntegral cellNumber) * 8
+        mask = (255 :: Word32) `shiftL` offset
+
+setCell ::
+    Bucket
+    -> Natural
+    -> FingerPrint
+    -> Bucket
+setCell (B bucket) cellNumber (FP fp) =
+    B $ bucket .|. mask
+    where
+        offset = (fromIntegral cellNumber) * 8
+        mask = (fromIntegral fp :: Word32) `shiftL` offset
 
 -- Initially going for correctness. Then measure it with benchmarks and tune it. Consider
 -- unpacking and alternative data structures.
@@ -105,7 +127,7 @@ insert cfilt@(F {numBuckets}) val = let
     bkts = buckets cfilt
     bucketA = bkts IM.! toIndex idxA
     in case insertBucket fp bucketA of
-        Just bucketA' -> cfilt {buckets = IM.insert (toIndex idxA) bucketA bkts}
+        Just bucketA' -> cfilt {buckets = IM.insert (toIndex idxA) bucketA' bkts}
         Nothing -> let
             idxB = secondaryIndex fp numBuckets idxA
             in bumpHash maxNumKicks cfilt idxB fp
@@ -127,23 +149,26 @@ insert cfilt@(F {numBuckets}) val = let
                     kickedIndex = kickedSecondaryIndex bumpedFP numBuckets idxB
                     in bumpHash (remaingKicks - 1) nextStepFilter kickedIndex bumpedFP
 
-        isBucketMinimum _ (B (a,b,c,d)) = let
+        isBucketMinimum _ bkt = let
+            a = getCell bkt 0
+            b = getCell bkt 1
+            c = getCell bkt 2
+            d = getCell bkt 3
             m = min a . min b $ min c d
             in (a == m, b == m, c == m, d == m)
-
 
 replaceInBucket ::
     FingerPrint
     -> (FingerPrint -> Bucket -> (Bool, Bool, Bool, Bool)) -- ^ Bucket predicate
     -> Bucket -- existing bucket
     -> (FingerPrint, Bucket) -- Removed fingerprint and latest bucket state
-replaceInBucket fp predicate bucket@(B (a,b,c,d)) = let
+replaceInBucket fp predicate bucket = let
     results = predicate fp bucket
     in case results of
-        (True, _, _, _) -> (a, B (fp, b, c, d))
-        (_, True, _, _) -> (b, B (a, fp, c, d))
-        (_, _, True, _) -> (c, B (a, b, fp, d))
-        (_, _, _, True) -> (d, B (a, b, c, fp))
+        (True, _, _, _) -> (getCell bucket 0, setCell bucket 0 fp)
+        (_, True, _, _) -> (getCell bucket 1, setCell bucket 1 fp)
+        (_, _, True, _) -> (getCell bucket 2, setCell bucket 2 fp)
+        (_, _, _, True) -> (getCell bucket 3, setCell bucket 3 fp)
         _ -> (fp, bucket)
 
 
@@ -164,7 +189,11 @@ member a cFilter =
         bB = buckets cFilter IM.! toIndex idxB
 
         -- fp `elem` [a,b,c,d] is simpler, but it allocates an additional list unnecessarily
-        inBucket fp (B (a,b,c,d)) = fp == a || fp == b || fp == c || fp == d
+        inBucket fp bucket =
+            fp == getCell bucket 0 ||
+            fp == getCell bucket 1 ||
+            fp == getCell bucket 2 ||
+            fp == getCell bucket 3
 
 
 --
@@ -174,13 +203,13 @@ member a cFilter =
 makeFingerprint :: Hashable a =>
     a
     -> FingerPrint
-makeFingerprint a = FP . fromIntegral $ hash a `mod` 255
+makeFingerprint a = FP . max 1 $  fromIntegral (natHash a) `mod` 255
 
 primaryIndex :: Hashable a =>
     a
     -> Natural
     -> IndexA
-primaryIndex a numBuckets = IA $ fromIntegral (hash a) `mod` numBuckets
+primaryIndex a numBuckets = IA $ natHash a `mod` numBuckets
 
 secondaryIndex ::
     FingerPrint
@@ -188,7 +217,7 @@ secondaryIndex ::
     -> IndexA
     -> IndexB
 secondaryIndex (FP fp) numBuckets (IA primary) =
-    IB . (`mod` numBuckets) $ primary `xor` fromIntegral (hash fp)
+    IB . (`mod` numBuckets) $ primary `xor` natHash fp
 
 kickedSecondaryIndex ::
     FingerPrint
@@ -196,7 +225,7 @@ kickedSecondaryIndex ::
     -> IndexB
     -> IndexB
 kickedSecondaryIndex (FP fp) numBuckets (IB alt) =
-    IB . (`mod` numBuckets) $ alt `xor` fromIntegral (hash fp)
+    IB . (`mod` numBuckets) $ alt `xor` natHash fp
 
 -- | Deletes a single occurance of 'a' from the 'Filter'. It first checks for a value
 -- in the primary index, then in the secondary index.
@@ -214,14 +243,17 @@ delete cFilt@(F {numBuckets, buckets}) a
         (removedFromA, bucketA') = removeFromBucket bucketA
         (_, bucketB') = removeFromBucket bucketB
         in if removedFromA
-           then cFilt {buckets = IM.insert (toIndex idxA) bucketA buckets}
+           then cFilt {buckets = IM.insert (toIndex idxA) bucketA' buckets}
            else cFilt {buckets = IM.insert (toIndex idxB) bucketB' buckets}
     where
         fp = makeFingerprint a
         idxA = primaryIndex fp numBuckets
         idxB = secondaryIndex fp numBuckets idxA
         -- TODO just use Control.Arrow
-        matchesFP _ (B (a,b,c,d))= (fp == a, fp == b, fp == c, fp == d)
+        matchesFP _ bucket = (fp == getCell bucket 0,
+                              fp == getCell bucket 1,
+                              fp == getCell bucket 2,
+                              fp == getCell bucket 3)
         removeFromBucket bucket = let
             (_, bucket') = replaceInBucket (FP 0) matchesFP  bucket
             in (bucket /= bucket', bucket')
@@ -230,11 +262,19 @@ insertBucket ::
     FingerPrint
     -> Bucket
     -> Maybe Bucket
-insertBucket fp (B (FP 0, a , b, c)) =  Just $ B (fp, a,b,c)
-insertBucket fp (B (a, FP 0, b, c)) = Just $ B (a, fp, b, c)
-insertBucket fp (B (a, b, FP 0, c)) = Just $ B (a, b, fp, c)
-insertBucket fp (B (a, b, c, FP 0)) = Just $ B (a,b,c, fp)
-insertBucket _ _ = Nothing
+insertBucket fp bucket =
+    case (a,b,c,d) of
+        (True, _, _, _) -> Just $ setCell bucket 0 fp
+        (_, True, _, _) -> Just $ setCell bucket 1 fp
+        (_, _, True, _) -> Just $ setCell bucket 2 fp
+        (_, _, _, True) -> Just $ setCell bucket 3 fp
+        _ -> Nothing
+    where
+        -- TODO factor out all of this duplicated code
+        a = emptyFP == getCell bucket 0
+        b = emptyFP == getCell bucket 1
+        c = emptyFP == getCell bucket 2
+        d = emptyFP == getCell bucket 3
 
 resize ::
     Filter a
@@ -242,3 +282,9 @@ resize ::
 resize old =  undefined
     -- double the size when capacity is maxed
     -- reinsert all elements
+    --
+
+natHash :: Hashable a =>
+    a ->
+    Natural
+natHash = fromIntegral . abs . hash
